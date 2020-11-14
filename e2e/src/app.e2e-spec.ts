f1f626e2aa1b16dfd6a6ec513dd17770178327f5
Gabriel Berlanda <gabriel.berlanda@gmail.com>
2020-11-14 01:13:30 -0300

    Rename project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tolentinos-challenge app is running!');
+    expect(page.getTitleText()).toEqual('angular-multi-range app is running!');
   });
 
   afterEach(async () => {
